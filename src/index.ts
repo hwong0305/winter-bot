ea4036940d22f8d1e60c03914e74b46d40f820de
hwong0305 <hwong.0305@gmail.com>
2022-02-22 22:38:29 -0800

    wip building weather

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,22 @@ import fetch from 'isomorphic-unfetch'
 import 'dotenv/config'
 
 const token = process.env.DISCORD_TOKEN
+const weatherDescToIconMap = {
+  'clear-day': '☀',
+  'clear-night': '',
+  rain: '🌧',
+  snow: '☃',
+  sleet: '🌃',
+  wind: '🌬',
+  fog: '🌁',
+  cloudy: '☁',
+  'partly-cloudy-day': '⛅',
+  'partly-cloudy-night': '☁',
+  hail: '🌨',
+  thunderstorm: '⛈',
+  tornado: '🌪'
+}
+
 console.log('Bot is starting...')
 
 const client = new Client({
@@ -24,16 +40,43 @@ client.on('interactionCreate', async interaction => {
     fetch(`https://wttr.in/${location}?format=j1`)
       .then(r => r.json())
       .then(async data => {
-        console.log(data)
-        console.log(data['current_condition'])
+        console.log(location, data['current_condition'])
+        const currentCondition = data['current_condition'][0]
+        const {
+          humidity,
+          temp_C,
+          temp_F,
+          FeelsLikeC,
+          FeelsLikeF,
+          windspeedKmph,
+          windspeedMiles
+        } = currentCondition
+        const weatherDescription = currentCondition.weatherDesc[0].value
+
+        const embed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(`Weather in ${location}`)
+          .addField('Currently', '')
+          .setTimestamp()
       })
       .catch(err => {
         console.log(err)
       })
-    // const payload = new MessagePayload(interaction.user, {})
-    // payload.data = await interaction.reply(MessagePayload(interaction.user))
+    /**
+     * Using MessageEmbed API
+     */
+
+    const embed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(`Weather in ${location}`)
+      .setTimestamp()
+      .addField(
+        'Currently',
+        ':cloud: Mostly Cloudy\n:thermometer: Temperature **5.5 C** (42F), Feels Like: **4.6 C** (40F)\n:wind_blowing_face: Wind 1.4 m/s (3.2mph)\n:sweat_drops: Humidity: 68%'
+      )
     // TODO: Default reply
     await interaction.reply('https://gfycat.com/concernedwelllitisopod')
+    // await interaction.reply({ embeds: [embed] })
   }
 })
 
